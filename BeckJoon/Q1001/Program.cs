0d3fb88434277ae8887748576c5a2cb2521cb4df
khj951213 <55070877+khj951213@users.noreply.github.com>
2023-09-18 17:10:18 +1000

    Q1001

---
+++ BeckJoon/Q1001/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+var inputs = Console.ReadLine();
+if (!string.IsNullOrEmpty(inputs))
+{
+    var a = int.Parse(inputs.Split(" ")[0]);
+    var b = int.Parse(inputs.Split(" ")[1]);
+    Console.WriteLine(a - b);
+}
---
+++ BeckJoon/Q1001/readme.md
@@ -0,0 +1,16 @@
+### Question 1001
+A - B
+
+### Example
+Input
+```
+3 2
+```
+Output
+```
+1
+```
+### Code
+```c#
+Console.WriteLine("Hello, World!");
+```
