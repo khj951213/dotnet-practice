3f712d09e400128e5a2d44531bd5856a225dc6dd
khj951213 <55070877+khj951213@users.noreply.github.com>
2023-09-18 23:29:29 +1000

    Solved Q11382

---
+++ BeckJoon/Q11382/Program.cs
@@ -0,0 +1,8 @@
+﻿var inputs = Console.ReadLine();
+if (!string.IsNullOrEmpty(inputs))
+{
+    var a = int.Parse(inputs.Split(" ")[0]);
+    var b = int.Parse(inputs.Split(" ")[1]);
+    var c = int.Parse(inputs.Split(" ")[2]);
+    Console.WriteLine(a + b + c);
+}
---
+++ BeckJoon/Q11382/readme.md
@@ -0,0 +1,16 @@
+### Question 11382
+A + B + C
+
+### Example
+Input
+```
+77 77 7777
+```
+Output
+```
+7931
+```
+### Code
+```c#
+Console.WriteLine("Hello, World!");
+```
