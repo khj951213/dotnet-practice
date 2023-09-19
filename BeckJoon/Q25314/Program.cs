043b9dbdf2caf81b70b171056f88a480546346a5
hyun <khj951213@gmail.com>
2023-09-19 23:01:49 +1000

    Solved Q2531

---
+++ BeckJoon/Q25314/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+
+var input = Console.ReadLine();
+
+if (!string.IsNullOrEmpty(input))
+{
+    var stringBuilder = new StringBuilder();
+    var n = int.Parse(input);
+    for (int i = 0; i < n/4; i++)
+    {
+        stringBuilder.Append("long ");
+    }
+    stringBuilder.Append("int");
+    Console.WriteLine(stringBuilder.ToString());
+}
