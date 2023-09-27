ba2b62b129a9a113b082d03faa64a59626cafefa
khj951213 <55070877+khj951213@users.noreply.github.com>
2023-09-27 15:08:31 +1000

    Deletage

---
+++ Delegate/PrintDelegate/PrintDelegate/Program.cs
@@ -0,0 +1,19 @@
+﻿PrintDelegateFunc PrintConsole = (string text) =>
+{
+    Console.WriteLine(text);
+};
+
+PrintDelegateFunc PrintFile = (string text) =>
+{
+    File.AppendAllText("../../../logs.txt", text);
+};
+
+static void ConnectToDatabase(PrintDelegateFunc log)
+{
+    log("Inserting a new record into a database.");
+    log("The record got inserted into the database.");
+}
+
+ConnectToDatabase(PrintFile);
+
+delegate void PrintDelegateFunc(string text);
