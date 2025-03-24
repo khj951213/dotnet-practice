2efb49e5d5d43eb5298f39bf29e60028ade17da3
June Dev <55070877+khj951213@users.noreply.github.com>
2025-03-25 10:59:46 +1100

    Update

---
+++ BeckJoon/Q2738/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Q2738
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(" ");
+            var n = int.Parse(input[0]);
+            var m = int.Parse(input[1]);
+
+            var nArr = new int[n, m];
+
+            for (int i = 0; i < n; i++)
+            {
+                var input2 = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+                for (int j = 0; j < m; j++)
+                {
+                    nArr[i, j] = input2[j];
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                var input2 = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+                for (int j = 0; j < m; j++)
+                {
+                    nArr[i, j] += input2[j];
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write(nArr[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
