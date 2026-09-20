aaa0918c845145aafd7b11f17d5bb51cb1756686
June Dev <55070877+khj951213@users.noreply.github.com>
2026-09-21 00:04:33 +1000

    Update

--- Algorithm/Algorithm/Problems/CorrectParatheses.cs
+++ Algorithm/Algorithm/Problems/CorrectParatheses.cs
@@ -4,38 +4,40 @@ public static class CorrectParantheses
 {
 
     public static bool Solve(string s)
-{
-    Stack<char> parentheses = new();
-
-    foreach (var c in s)
     {
-        if (c == ')')
-        {
-            if (parentheses.Count == 0 || parentheses.Peek() != '(')
-                return false;
+        Stack<char> parentheses = new();
 
-            parentheses.Pop();
-        }
-        else if (c == ']')
+        foreach (var c in s)
         {
-            if (parentheses.Count == 0 || parentheses.Peek() != '[')
-                return false;
+            if (c == ')')
+            {
+                // peek는 stack의 맨 위 값을 보는 메서드
+                // peek는 null일 경우 예외처리가되기 때문에 확인
+                if (parentheses.Count == 0 || parentheses.Peek() != '(')
+                    return false;
 
-            parentheses.Pop();
-        }
-        else if (c == '}')
-        {
-            if (parentheses.Count == 0 || parentheses.Peek() != '{')
-                return false;
+                parentheses.Pop();
+            }
+            else if (c == ']')
+            {
+                if (parentheses.Count == 0 || parentheses.Peek() != '[')
+                    return false;
 
-            parentheses.Pop();
+                parentheses.Pop();
+            }
+            else if (c == '}')
+            {
+                if (parentheses.Count == 0 || parentheses.Peek() != '{')
+                    return false;
+
+                parentheses.Pop();
+            }
+            else if (c == '(' || c == '{' || c == '[')
+            {
+                parentheses.Push(c);
+            }
         }
-        else if (c == '(' || c == '{' || c == '[')
-        {
-            parentheses.Push(c);
-        }
+
+        return parentheses.Count == 0; // 추가적인 괄호가 (예:'(((') 들어있으면 return false;
     }
-
-    return parentheses.Count == 0;
-}
 }
